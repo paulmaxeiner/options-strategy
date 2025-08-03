4205f46e9c5f84455a719bfcbf8d3f71a6565794
paulmaxeiner <maxeiner@wustl.edu>
2025-08-03 16:09:54 -0400

    Added RSI

--- src/app.py
+++ src/app.py
@@ -25,6 +25,19 @@ def black_scholes_call(S,T,K):
     call_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
     return call_price
 
+def calculate_rsi(data, period=14):
+    delta = data.diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.ewm(com=period - 1, adjust=False).mean()
+    avg_loss = loss.ewm(com=period - 1, adjust=False).mean()
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+
 
 
 
@@ -65,14 +78,14 @@ data['T'] = data.apply(lambda row: minutes_to_4pm(row['Datetime']), axis=1)
 ##data.columns = ['_'.join(col).strip() if isinstance(col, tuple) else col for col in df.columns]
 data.columns = [' '.join(col).strip() for col in data.columns.values]
 data['Call Price 625'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],625), axis=1)
-
+data['RSI'] = calculate_rsi(data['Close SPY'])
 data['Call Price 623'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],623), axis=1)
 data['Call Price 624'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],624), axis=1)
 data['Call Price 626'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],626), axis=1)
 data['Call Price 630'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],630), axis=1)
 data['Call Price 628'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],628), axis=1)
 st.write(data)
-
+st.line_chart(data,x='Datetime',y=['RSI','Call Price 625'])
 
 st.line_chart(data,x='Datetime',y='Close SPY')
 st.line_chart(data,x='Datetime',y=['Call Price 625','Call Price 626','Call Price 623','Call Price 624','Call Price 630','Call Price 628'])

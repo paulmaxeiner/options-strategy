bd20baae8c843d381aa0230e1245bb246d732c1f
paulmaxeiner <maxeiner@wustl.edu>
2025-08-31 13:00:51 -0500

    Implement pandas df

--- src/app.py
+++ src/app.py
@@ -28,6 +28,10 @@ def black_scholes_call(S,T,K):
     call_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
     return call_price
 
+
+
+
+
 def calculate_rsi(data, period=14):
     delta = data.diff()
     gain = delta.where(delta > 0, 0)
@@ -42,50 +46,70 @@ def calculate_rsi(data, period=14):
 
 
 
+def minutes_to_close(datetime_series):
+    close_time = 16
+    datetime_series = datetime_series.dt.tz_convert('America/New_York')
+    four_pm_today = datetime_series.dt.normalize() + pd.Timedelta(hours=close_time)
+    time_difference = four_pm_today - datetime_series
+    minutes = time_difference.dt.total_seconds() / 60
+
+    return minutes /60/24/365
 
 
 
-dt1_str = "2025-07-15 20:30:00+00:00"
+dt1_str = "2025-08-14 20:30:00+00:00"
 est = pytz.timezone('US/Eastern')
 utc = pytz.utc
 fmt = '%Y-%m-%d %H:%M:%S %Z%z'
 dt1 = datetime.fromisoformat(dt1_str)
 
 
-data = yf.download('SPY', start='2025-08-14', end='2025-08-15', interval='1m')
+data = yf.download(['SPY','^VIX'], start='2025-08-14', end='2025-08-15', interval='1m')
+
+data = data.dropna()
 data = data.reset_index()
 
-vix = yf.download('^VIX', start='2025-08-14', end='2025-08-15', interval='1m').reset_index()
-vix['Datetime'] = pd.to_datetime(vix['Datetime'], utc=True)
-st.write(vix)
-
-data['Date'] = data['Datetime'].dt.strftime(fmt)
-data['Pct Change']=data['Close'].pct_change()
-
 data['Time'] = pd.to_datetime(data['Datetime'], utc=True, format='%H:%M:%S.%f').dt.tz_convert('America/New_York').dt.time
-st.write(data)
 
-def minutes_to_4pm(datetime_series):
-    datetime_series = datetime_series.dt.tz_convert('America/New_York')
-    four_pm_today = datetime_series.dt.normalize() + pd.Timedelta(hours=16)
-    time_difference = four_pm_today - datetime_series
-    minutes = time_difference.dt.total_seconds() / 60
+spy = pd.DataFrame(data={
+    'Datetime': data['Datetime'],
+    'Time': data['Time'],
+    'Close': data['Close']['SPY'],
+    'Open': data['Open']['SPY'],
+    'High': data['High']['SPY'],
+    'Low': data['Low']['SPY'],
+    'Volume': data['Volume']['SPY'],
+    'Return': data['Close']['SPY'].pct_change()
+})
 
-    return minutes /60/24/365
 
-data['T'] = data.apply(lambda row: minutes_to_4pm(row['Datetime']), axis=1)
+spy['Datetime'] = pd.to_datetime(spy['Datetime'], utc=True)
+spy['Time'] = pd.to_datetime(data['Datetime'], utc=True, format='%H:%M:%S.%f').dt.tz_convert('America/New_York').dt.time
+spy['T'] = data.apply(lambda row: minutes_to_close(row['Datetime']), axis=1)
+st.write(spy)
 
 
+atm_price = round(spy['Close'].iloc[-1])
+st.write("ATM Price:", atm_price)
+
+call_prices = pd.DataFrame({
+    atm_price + x: spy.apply(lambda row: black_scholes_call(row['Close'], row['T'], atm_price + x), axis=1)
+    for x in range(-5, 5)
+})
+
+st.write(call_prices)
+
+st.line_chart(call_prices)
+
+
+
+
+data['T'] = data.apply(lambda row: minutes_to_close(row['Datetime']), axis=1)
 
 ##data.columns = ['_'.join(col).strip() if isinstance(col, tuple) else col for col in df.columns]
 data.columns = [' '.join(col).strip() for col in data.columns.values]
-data['Call Price 625'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],625), axis=1)
 data['RSI'] = calculate_rsi(data['Close SPY'])
-data['Call Price 623'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],623), axis=1)
-data['Call Price 624'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],624), axis=1)
-data['Call Price 626'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],626), axis=1)
-data['Call Price 630'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],630), axis=1)
-data['Call Price 628'] = data.apply(lambda row:black_scholes_call(row['Close SPY'],row['T'],628), axis=1)
+
 
 
 ### Actual strategy backtest
@@ -97,6 +121,7 @@ def rsi_signal(rsi):
         return -1    # Sell signal
     else:
         return 0     # Hold
+
 
 data['Signal'] = data['RSI'].apply(rsi_signal)
 
@@ -136,10 +161,6 @@ ax.plot(data['Close SPY'], label='Market Return')
 st.pyplot(fig)
 st.pyplot(fig)
 
-st.line_chart(data,x='Datetime',y=['RSI','Call Price 625'])
-
-st.line_chart(data,x='Datetime',y='Close SPY')
-st.line_chart(data,x='Datetime',y=['Call Price 625','Call Price 626','Call Price 623','Call Price 624','Call Price 630','Call Price 628'])
 
 
 
